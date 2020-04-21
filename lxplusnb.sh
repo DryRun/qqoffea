594aa7828af2df9632ac7aa06b59db1d42ee55ef
DryRun <david.renhwa.yu@gmail.com>
2020-04-21 01:32:07 -0500

    Add tunnel script

--- lxplusnb.sh
+++ lxplusnb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Tunneling to ${1}, port ${2}"
-ssh -v -N -L localhost:${2}:localhost:${2} dryu@lxplus${1}.cern.ch
+ssh -v -N -L localhost:${2}:localhost:${2} $(whoami)@lxplus${1}.cern.ch
